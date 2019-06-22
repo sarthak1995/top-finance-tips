739693e1b5f71acf2106de105e50ab800529f17c
Sarthak Aggarwal <sarthak.ag95@gmail.com>
2019-06-22 16:45:29 +0530

    Updates

---
+++ precache-manifest.4e6d0bada1f55a33235afcbc877bbb52.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b8cbdc7f739e781b069819dcc22a166",
+    "url": "/sarthak1995/top-finance-tips/index.html"
+  },
+  {
+    "revision": "908458f89c168296497e",
+    "url": "/sarthak1995/top-finance-tips/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/sarthak1995/top-finance-tips/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "908458f89c168296497e",
+    "url": "/sarthak1995/top-finance-tips/static/js/main.7a58bd6a.chunk.js"
+  },
+  {
+    "revision": "a177cbbfb5392c7a3e64",
+    "url": "/sarthak1995/top-finance-tips/static/js/runtime~main.23cca38f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sarthak1995/top-finance-tips/static/media/logo.5d5d9eef.svg"
+  }
+]);
