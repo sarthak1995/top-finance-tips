d2207e0bec800f2a588826be2a74d6a50f2cda6a
Sarthak Aggarwal <sarthak.ag95@gmail.com>
2019-06-22 16:44:37 +0530

    inital commit

---
+++ src/Content/Content.js
@@ -0,0 +1,10 @@
+import React,{Component} from 'react'
+
+const content = (props) => (
+    <React.Fragment>
+        <h1>Top Financial Tips</h1>
+        <br/>
+    </React.Fragment>
+)
+
+export default content;
